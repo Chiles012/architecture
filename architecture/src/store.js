5a5505a69144add45444dffb132530ae6fb602be
Chiles012 <sersan.christian@gmail.com>
2021-11-03 21:02:20 -0600

    carpetas

---
+++ architecture/src/store.js
@@ -0,0 +1,24 @@
+/**
+ * El store es donde se contiene el reducer
+ * * compose se usa para crear muchos reducer
+ * * applyMiddleware nos proporciona la utilidad para intervenir dentro de la accion thunk es un middleware
+ * * thunk es un middleware para poder usar peticiones asyncronas dentro de los actions sin el no es posible hacerlo
+ * * reducer es el conglomerado de los reducer que se usaran dentro del proyecto
+ */
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import reducer from './reducer';
+
+const store = createStore(
+    reducer,
+    compose( 
+        applyMiddleware(
+            thunk
+            ),
+            typeof window === 'object' && 
+                typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ?
+                    window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+    )
+)
+
+export default store;
---
+++ architecture/src/util/axios.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: "" // * Ingresar la base url de la api ejemplo https://localhost:3000/api/v1/
+})
